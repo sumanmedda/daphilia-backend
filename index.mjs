527fe16e3c25bb3f9837d273461cb1fc531e6600
sumanmedda <sumanmedda33@gmail.com>
2024-09-09 19:17:37 +0530

    intial

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import express from "express"
+import bodyParser from "body-parser"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
-import bodyParser from "body-parser"
 import userRoute from "./users/userRoute/userRoute.mjs"
 
 const app = express()
